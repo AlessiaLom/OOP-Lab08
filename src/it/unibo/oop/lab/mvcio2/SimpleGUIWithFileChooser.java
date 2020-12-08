b1fe48f526f1b2a30791275703d9514376fbb88e
Barbara Mazzoni <alessia.lombardi4@studio.unibo.it>
2020-12-08 16:38:31 +0100

    Finito es mvcio2

--- src/it/unibo/oop/lab/mvcio2/SimpleGUIWithFileChooser.java
+++ src/it/unibo/oop/lab/mvcio2/SimpleGUIWithFileChooser.java
@@ -1,5 +1,24 @@
 package it.unibo.oop.lab.mvcio2;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.LayoutManager;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import it.unibo.oop.lab.mvcio.Controller;
+
 /**
  * A very simple program using a graphical interface.
  * 
@@ -31,5 +50,72 @@ public final class SimpleGUIWithFileChooser {
      * update the UI: in this example the UI knows when should be updated, so
      * try to keep things separated.
      */
+    private final JFrame frame = new JFrame();
 
+    public SimpleGUIWithFileChooser(final Controller c) {
+        final JTextArea text = new JTextArea();
+        final JButton save = new JButton("Save");
+        final JPanel panel = new JPanel();
+        final LayoutManager bl = new BorderLayout();
+        panel.setLayout(bl);
+        panel.add(text, BorderLayout.CENTER);
+        panel.add(save, BorderLayout.SOUTH);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        save.addActionListener(new ActionListener() {
+            public void actionPerformed(final ActionEvent e) {
+                try {
+                    c.saveContent(text.getText());
+                } catch (IOException error) {
+                    System.out.println("Error");
+                }
+            }
+        });
+
+        final JTextField currentPath = new JTextField(c.getPath());
+        currentPath.setEditable(false);
+        final JButton browse = new JButton("Browse...");
+        final JPanel panel2 = new JPanel();
+        panel2.setLayout(new BorderLayout());
+        panel2.add(currentPath, BorderLayout.CENTER);
+        panel2.add(browse, BorderLayout.LINE_END);
+        panel.add(panel2, BorderLayout.NORTH);
+        frame.setContentPane(panel);
+
+        browse.addActionListener(new ActionListener() {
+
+            public void actionPerformed(final ActionEvent e) {
+                final JFileChooser fileChooser = new JFileChooser();
+                fileChooser.setSelectedFile(c.getCurrentFile());
+                final int result = fileChooser.showSaveDialog(frame);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    final File dest = fileChooser.getSelectedFile();
+                    c.setDestination(dest);;
+                    currentPath.setText(dest.getPath());
+                }
+                else if (result == JFileChooser.CANCEL_OPTION) {
+                }
+                else {
+                    JOptionPane.showMessageDialog(frame, result, "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / 2, sh / 2);
+
+        frame.setLocationByPlatform(true);
+    }
+
+    public void display() {
+        frame.setVisible(true);
+    }
+
+    public static void main(final String... a) {
+        final SimpleGUIWithFileChooser simplegui = new SimpleGUIWithFileChooser(new Controller());
+        simplegui.display();
+    }
 }
+
